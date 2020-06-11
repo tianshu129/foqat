66dd8e2401a8b40f0ee4b295cbc516668df79e8b
Tianshu Chen <tianshu129@163.com>
2020-06-11 02:49:46 -0600

    v0.5.0 added function tuv.

---
+++ R/tuv.R
@@ -0,0 +1,51 @@
+#' Calculate TUV in batch.
+#'
+#' @param pathtuv path for TUV folder such as "c:/tuv".
+#' @param df dataframe for variable.
+#' @param colt column index of date.
+#' @return result_j dataframe for TUV result. 
+#' @export
+#' @importFrom stringr str_split_fixed
+#' @importFrom lubridate hour
+
+tuv <- function(pathtuv, df, colt){
+outfile="usrout.txt"
+#Store the original work path
+oldwd=getwd()
+#Set the TUV directory to the current working path
+setwd(paste(c(pathtuv,"/tuv"),collapse =""))
+#df<- data.frame(df,stringsAsFactors = FALSE)
+#set date to first column
+if(colt!=1){df[,c(1,colt)] <- df[,c(colt,1)]}
+# read a row at a time
+for(irow in 1:nrow(df)){
+	#Write date
+	iyear=format(df[irow,1],'%Y')
+	imonth=format(df[irow,1],'%m')
+	iday=format(df[irow,1],'%d')
+	cmdtxt=paste(c("iyear\n", iyear, "\nimonth\n", imonth, "\niday\n", iday, "\n"),collapse ="")		
+	# write other parameters
+	for(icol in 2:ncol(df)){		 		 
+		#combine part of the cmd
+		cmdtxt=paste(c(cmdtxt, colnames(df)[icol], "\n", df[irow,icol], "\n"),collapse ="")
+	}
+	#Finally, add the selection mode at the beginning and the three return commands at the end
+	cmdtxt=paste(c("\n2\n",cmdtxt,"\n\n\n"),collapse ="")
+	system(command="tuv", input=cmdtxt)
+	#Read usrout.txt
+	result <- data.frame(readLines(paste(c(pathtuv,"/", outfile),collapse ="")), stringsAsFactors = FALSE)
+	result_j <- data.frame(result[(which(grepl("deg.", result[,1]))[2]+1):(nrow(result)-1),])
+	result_j_df <- data.frame(str_split_fixed(result_j[,1], "\\s+", 10), stringsAsFactors = FALSE)[,2:10]
+	colnames(result_j_df) <- c("time hrs.", "sza deg.", "1", "2", "3", "4", "5", "6", "7")
+	result_j_df[,1] <- as.character(hours(as.numeric(result_j_df[,1])) + as.Date(df[irow,1]))
+	# Writes result data to the result df
+	if(exists("result_j_df_all")){
+		result_j_df_all <- rbind(result_j_df_all, result_j_df)
+	}else{
+		result_j_df_all <- result_j_df
+	}
+}
+# Restore the working path
+setwd(oldwd)
+result_j <- result_j_df_all
+}
