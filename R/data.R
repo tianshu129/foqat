642f6f31e1836ae97a29791c75ec7e432657a3b0
Tianshu Chen <tianshu129@163.com>
2020-06-28 11:59:41 -0600

    v0.5.19 added demo data

---
+++ R/data.R
@@ -0,0 +1,51 @@
+#' Demo data of air quality
+#'
+#' 5 days air quality data (1 min) includes: NO, NO2, CO, SO2, O3.
+#' The variables are as follows:
+#'
+#' @format A data frame with 7140 rows and 6 variables:
+#' \describe{
+#'   \item{Time}{Time for data}
+#'   \item{NO}{Nitric oxide (NO)}
+#'   \item{NO2}{Nitrogen Dioxide (NO2)}
+#'   \item{CO}{Carbon monoxide (CO)}
+#'   \item{SO2}{Sulfur dioxide (SO2)}
+#'   \item{O3}{Ozone (O3)}
+#' }
+"aqi"
+
+#' Demo data of meteorology
+#'
+#' 5 days meteorology data (5 mins) includes: Temperature, Humidity,
+#' Wind speed, Wind direction.
+#' The variables are as follows:
+#'
+#' @format A data frame with 1287 rows and 5 variables:
+#' \describe{
+#'   \item{Time}{Time for data}
+#'   \item{TEM}{Temperature}
+#'   \item{HUM}{Humidity}
+#'   \item{WS}{Wind speed}
+#'   \item{WD}{Wind direction}
+#'   }
+"met"
+
+#' Demo data of volatile organic compounds (VOCs)
+#'
+#' 5 days VOCs data (1 hour) includes: Propylene, Acetylene, n-Butane,
+#' trans-2-Butene, 2-Methylpentane, 3-Methylpentane, 2-Methylhexane, Cyclohexane.
+#' The variables are as follows:
+#'
+#' @format A data frame with 120 rows and 9 variables:
+#' \describe{
+#'   \item{Time}{Time for data}
+#'   \item{Propylene}{Propylene}
+#'   \item{Acetylene}{Acetylene}
+#'   \item{n-Butane}{n-Butane}
+#'   \item{trans-2-Butene}{trans-2-Butene}
+#'   \item{2-Methylpentane}{2-Methylpentane}
+#'   \item{3-Methylpentane}{3-Methylpentane}
+#'   \item{2-Methylhexane}{2-Methylhexane}
+#'   \item{Cyclohexane}{Cyclohexane}
+#' }
+"voc"
